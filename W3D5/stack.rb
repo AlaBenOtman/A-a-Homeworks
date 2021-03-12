94bc9a0f548486530ab2e8644412c5ea0b610bb8
voldemort <alabenothman@gmail.com>
2021-03-11 19:14:54 -0800

    Created some codes for LIFO

--- W3D5/stack.rb
+++ W3D5/stack.rb
@@ -1,17 +1,17 @@
 class Stack
     def initialize
-      # create ivar to store stack here!
+      @stacks = []
     end
 
     def push(el)
-      # adds an element to the stack
+        @stacks << el
     end
 
     def pop
-      # removes one element from the stack
+      @stacks.pop
     end
 
     def peek
-      # returns, but doesn't remove, the top element in the stack
+      @stacks 
     end
   end
